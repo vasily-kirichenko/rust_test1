d976a75ee63a55dc78a48b02f3e475ae356883ad
Vasily Kirichenko <vaskir@gmail.com>
2015-04-12 22:28:58 +0300

    parallel fib

--- src/fib.rs
+++ src/fib.rs
@@ -1,20 +1,26 @@
 use std::sync::*;
 
-fn sfib(n: i32) -> i32 {
+fn sfib(n: i64) -> i64 {
     match n {
         0 | 1 | 2 => n,
  		_ => sfib (n-1) + sfib(n-2)
  	}
 }
 
-pub fn fib(n: i32) -> Future<i32> {
-    Future::spawn(move || {
-        if n < 13 { sfib(n) }
-        else {
-            let mut n2f = fib(n - 2);
-            let n1 = fib(n - 1).get();
-            let n2 = n2f.get();
-            n1 + n2
-        }
-    })
+pub fn fib(n: i64) -> (i64, i32) {
+    let mut fc = &0i32;
+
+    fn cfib(n: i64) -> Future<i64> {
+        //*fc = *fc + 1;
+        Future::spawn(move || {
+            if n < 13 { sfib(n) }
+            else {
+                let mut n2f = cfib(n - 2);
+                let n1 = cfib(n - 1).get();
+                let n2 = n2f.get();
+                n1 + n2
+            }
+        })
+    }
+    (cfib(n).get(), *fc)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,53 @@
-//#![feature(custom_derive)]
+#![feature(test)]
 #![feature(std_misc)]
+#![feature(slice_patterns)]
 
-//pub mod consequtive_ints;
-use std::fmt::Debug;
+pub mod fib;
 
-mod fib;
+//extern crate crypto;
+extern crate time;
+extern crate test;
+extern crate getopts;
 
-// fn print_array_by_ref<T: Debug>(xs: &[T]) {
-// 	for x in xs.iter() {
-// 		println!("{:?}", x)
-// 	}
-// }
-//
-// fn print_boxed_array<T: Debug>(xs: Box<[T]>) {
-// 	for x in xs.iter() {
-// 		println!("{:?}", x)
-// 	}
+//use crypto::md5::Md5;
+//use crypto::sha1::Sha1;
+//use crypto::sha2::{Sha256, Sha512};
+//use crypto::digest::Digest;
+use time::now;
+use time::Duration;
+use std::env;
+
+fn tc<R, F: Fn() -> R>(f: F) -> (R, Duration) {
+  let started = time::now();
+  let r = f();
+  (r, time::now() - started)
+}
+
+// fn bench<T: Digest>(name: &str, digest: &mut T, input: &Vec<u8>) {
+// 	let started = time::now();
+//     for _ in 0..10 {
+// 		digest.input(&input);
+// 		println!("size = {}, hash = {}", input.len(), digest.result_str());
+// 		digest.reset();
+//     }
+
+//     let elapsed = time::now() - started;
+//     println!("{:?} elapled {}", name, elapsed);
 // }
 
 fn main() {
-	let n = 30;
-	println!("fib({}) = {}", n, fib::fib(n).get());
+    let args: Vec<String> = env::args().collect();
+    let n: i64 =
+        match args.as_slice() {
+            [_, nStr, ..] => nStr,
+            _ => panic!("wrong arguments.")
+        }.parse().unwrap();
+    let ((r, futureCount), elapsed) = tc(|| { fib::fib(n) });
+    println!("fib({}) = {}, elapsed: {}", n, r, elapsed);
 
-	// let a = [1, 2, 3];
-	// print_array_by_ref(&a);
-	//
-	// let b = Box::new(a);
-	// print_boxed_array(b);
-	// print_array_by_ref(&a);
+    //let input = vec![0u8; 100000000];
+    //bench("MD5", &mut Md5::new(), &input);
+    //bench("SHA1", &mut Sha1::new(), &input);
+    //bench("SHA256", &mut Sha256::new(), &input);
+    //test::black_box(bench("SHA512", &mut Sha512::new(), &input));
 }
